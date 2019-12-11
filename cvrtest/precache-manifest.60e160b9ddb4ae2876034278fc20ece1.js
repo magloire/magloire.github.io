aa520dcf400ba6a0686aff29fd2a92f97cb2e17e
Magloire Sendegeya <magloire1@gmail.com>
2019-12-11 13:30:56 +0100

    cvrtest upload
    cvrtest upload

---
+++ cvrtest/precache-manifest.60e160b9ddb4ae2876034278fc20ece1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3ef7982a15f99624af28db2db5e65c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3f5bb268cdb8f12c242",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "502131722688eb3aa82d",
+    "url": "/static/js/2.fb685e6d.chunk.js"
+  },
+  {
+    "revision": "d3f5bb268cdb8f12c242",
+    "url": "/static/js/main.9ada4333.chunk.js"
+  },
+  {
+    "revision": "957c98f130eb8e1eb912",
+    "url": "/static/js/runtime-main.e7b982e6.js"
+  }
+]);
