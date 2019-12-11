42302b829d6e1cc93b32b3b7032f2f2433f54fa6
Magloire Sendegeya <magloire1@gmail.com>
2019-12-11 13:51:34 +0100

    change to cvrtest

--- cvrtest/service-worker.js
+++ cvrtest/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60e160b9ddb4ae2876034278fc20ece1.js"
+  "/cvrtest/precache-manifest.17320ff587819004122efef986dee1d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cvrtest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
